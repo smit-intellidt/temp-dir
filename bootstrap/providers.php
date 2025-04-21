ef7c203d9058cbe82247f95ec12cfb24fa852fb4
smit-intellidt <smitd@intellidt.com>
2025-04-21 16:25:49 +0530

    removed dinnerapp 11

--- bootstrap/providers.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    App\Providers\AppServiceProvider::class,
-    Barryvdh\DomPDF\ServiceProvider::class,
-    Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
-    Spatie\Permission\PermissionServiceProvider::class
-];
